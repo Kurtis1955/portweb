8b278c6055ed69a9eca5872ac471941a2c9e735c
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-03-21 18:36:49 -0700

    Create page.js

---
+++ app/pages/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../page.module.scss'
+
+function page(props) {
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+}
+
+export default page;
