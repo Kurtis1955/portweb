90ec0ee33c75ad39dd8b16bf8d024713e54e3fb2
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-03-24 16:50:43 -0700

    Update page.js

--- app/pages/project-build/page.js
+++ app/pages/project-build/page.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import { SiReact, SiFlutter} from "react-icons/si";
+import {FaGlobe, FaFileCode, FaNodeJs,FaMobileAlt} from "react-icons/fa";
+import { GiTeacher } from "react-icons/gi";
+import { IoLogoApple } from "react-icons/io";
+import { CiMobile2 } from "react-icons/ci";
+import { RiArrowRightLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { TbShieldCode } from "react-icons/tb";
+import { IoLogoAndroid } from "react-icons/io5";
+import { CgWebsite } from "react-icons/cg";
+import { IoLogoDesignernews } from "react-icons/io";
 
 const Projects_build = () => {
   return (
@@ -9,36 +20,6 @@ const Projects_build = () => {
         </h1>
 
         <div className="  w-[100%] grid gap-0 grid-cols-3 justify-center">
-          <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
-            <div className="flex items-center gap-4 mb-6 ">
-              <FaGlobe size={62} color="#02007f" />{" "}
-              <h2 className="text-2xl font-[700]">
-                Web <br /> Development
-              </h2>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <span className="flex gap-4 pl-4  items-center text-lg font-[200]">
-                <FaNodeJs color="green" size={25} /> NodeJs
-              </span>
-
-              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
-                <TbBrandNextjs color="black" size={25} /> NextJS{" "}
-              </span>
-
-              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
-                <SiReact color="LIGHTBLUE" size={25} /> React{" "}
-              </span>
-
-              <button
-                className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
-          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
-              >
-                Read More <RiArrowRightLine />
-              </button>
-            </div>
-          </div>
-
           <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
             <div className="flex items-center gap-4 mb-6 ">
               <FaMobileAlt size={62} color="#02007f" />{" "}
@@ -64,6 +45,36 @@ const Projects_build = () => {
                 <SiFlutter color="LIGHTBLUE" size={25} /> Flutter{" "}
               </span>
 
+
+              <button
+                className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
+          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
+              >
+                Read More <RiArrowRightLine />
+              </button>
+            </div>
+          </div>
+
+          <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
+            <div className="flex items-center gap-4 mb-6 ">
+              <FaGlobe size={62} color="#02007f" />{" "}
+              <h2 className="text-2xl font-[700]">
+                Web <br /> Development
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <span className="flex gap-4 pl-4  items-center text-lg font-[200]">
+                <FaNodeJs color="green" size={25} /> NodeJs
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <TbBrandNextjs color="black" size={25} /> NextJS{" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <SiReact color="LIGHTBLUE" size={25} /> React{" "}
+              </span>
 
               <button
                 className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
