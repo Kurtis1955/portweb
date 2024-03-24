be40025fc600506ea1f7a64063a28aea7daeac10
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-03-24 16:42:15 -0700

    Update page.js

--- app/pages/project-build/page.js
+++ app/pages/project-build/page.js
@@ -74,7 +74,46 @@ const Projects_build = () => {
             </div>
           </div>
 
+          <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
+            <div className="flex items-center gap-4 mb-6 ">
+              <GiTeacher size={62} color="#02007f" />{" "}
+              <h2 className="text-2xl font-[700]">
+               TECH <br /> Training
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <span className="flex gap-4 pl-4  items-center text-lg font-[200]">
+                <CgWebsite  color="green" size={25} /> Web Development{" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <FaFileCode  color="black" size={25} /> Frontend Development {" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <TbShieldCode color="LIGHTBLUE" size={25} /> Backend Development
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <IoLogoDesignernews  color="LIGHTBLUE" size={25} /> Graphic Designing
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                {" "}
+                <CiMobile2  color="LIGHTBLUE" size={25} />
+              Mobile App Development
+              </span>
+
+              <button
+                className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
+          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
+              >
+                Read More <RiArrowRightLine />
+              </button>
+            </div>
           </div>
+    </div>
     </section>
     </div>
   )
