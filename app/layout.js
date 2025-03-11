9c6f579509fe1c239edf9201fcb3eab60eb1c16f
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2025-03-11 17:11:57 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <body>
       <Navbar/>
       {children}
       <FooterPage/>
+      </body>
     </html>
   )
 }
