876a65fd13437d52f59cf11918667a28c8520229
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2025-03-13 15:18:09 +0000

    project pictures

--- app/pages/project-build/page.js
+++ app/pages/project-build/page.js
@@ -13,14 +13,13 @@ import { IoLogoDesignernews } from "react-icons/io";
 
 const Projects_build = () => {
   return (
-    <div>
-       <section className=" bg-[white] relative py-20 px-20 w-[100vw] " >
+    <div className='w-full px-32 py-14 bg-amber-200'>
         <h1 id="services" className="text-center ">
-          Services We Offer
+          Recent Projects
         </h1>
 
-        <div className="  w-[100%] grid gap-0 grid-cols-3 justify-center">
-          <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
+        <div className="w-full bg-amber-600 grid gap-8 grid-cols-2">
+          <div className="w-full bg-red-600 mb-6 shadow p-10 rounded-xl pl-4 ">
             <div className="flex items-center gap-4 mb-6 ">
               <FaMobileAlt size={62} color="#02007f" />{" "}
               <h2 className="text-2xl font-[700]">
@@ -48,14 +47,14 @@ const Projects_build = () => {
 
               <button
                 className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
-          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
+                px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
               >
                 Read More <RiArrowRightLine />
               </button>
             </div>
           </div>
 
-          <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
+          <div className="w-full mb-6 bg-red-600 shadow p-10 rounded-xl pl-4 ">
             <div className="flex items-center gap-4 mb-6 ">
               <FaGlobe size={62} color="#02007f" />{" "}
               <h2 className="text-2xl font-[700]">
@@ -85,7 +84,7 @@ const Projects_build = () => {
             </div>
           </div>
 
-          <div className="w-[28vw] mb-6 shadow p-10 rounded-xl pl-4 ">
+          <div className="w-full mb-6 bg-red-600 shadow p-10 rounded-xl pl-4 ">
             <div className="flex items-center gap-4 mb-6 ">
               <GiTeacher size={62} color="#02007f" />{" "}
               <h2 className="text-2xl font-[700]">
@@ -124,8 +123,124 @@ const Projects_build = () => {
               </button>
             </div>
           </div>
-    </div>
-    </section>
+          <div className="w-full mb-6 bg-red-600 shadow p-10 rounded-xl pl-4 ">
+            <div className="flex items-center gap-4 mb-6 ">
+              <GiTeacher size={62} color="#02007f" />{" "}
+              <h2 className="text-2xl font-[700]">
+               TECH <br /> Training
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <span className="flex gap-4 pl-4  items-center text-lg font-[200]">
+                <CgWebsite  color="green" size={25} /> Web Development{" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <FaFileCode  color="black" size={25} /> Frontend Development {" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <TbShieldCode color="LIGHTBLUE" size={25} /> Backend Development
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <IoLogoDesignernews  color="LIGHTBLUE" size={25} /> Graphic Designing
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                {" "}
+                <CiMobile2  color="LIGHTBLUE" size={25} />
+              Mobile App Development
+              </span>
+
+              <button
+                className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
+          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
+              >
+                Read More <RiArrowRightLine />
+              </button>
+            </div>
+          </div>
+          <div className="w-full mb-6 bg-red-600 shadow p-10 rounded-xl pl-4 ">
+            <div className="flex items-center gap-4 mb-6 ">
+              <GiTeacher size={62} color="#02007f" />{" "}
+              <h2 className="text-2xl font-[700]">
+               TECH <br /> Training
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <span className="flex gap-4 pl-4  items-center text-lg font-[200]">
+                <CgWebsite  color="green" size={25} /> Web Development{" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <FaFileCode  color="black" size={25} /> Frontend Development {" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <TbShieldCode color="LIGHTBLUE" size={25} /> Backend Development
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <IoLogoDesignernews  color="LIGHTBLUE" size={25} /> Graphic Designing
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                {" "}
+                <CiMobile2  color="LIGHTBLUE" size={25} />
+              Mobile App Development
+              </span>
+
+              <button
+                className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
+          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
+              >
+                Read More <RiArrowRightLine />
+              </button>
+            </div>
+          </div>
+          <div className="w-full mb-6 bg-red-600 shadow p-10 rounded-xl pl-4 ">
+            <div className="flex items-center gap-4 mb-6 ">
+              <GiTeacher size={62} color="#02007f" />{" "}
+              <h2 className="text-2xl font-[700]">
+               TECH <br /> Training
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <span className="flex gap-4 pl-4  items-center text-lg font-[200]">
+                <CgWebsite  color="green" size={25} /> Web Development{" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <FaFileCode  color="black" size={25} /> Frontend Development {" "}
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <TbShieldCode color="LIGHTBLUE" size={25} /> Backend Development
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                <IoLogoDesignernews  color="LIGHTBLUE" size={25} /> Graphic Designing
+              </span>
+
+              <span className="flex gap-4 pl-4 items-center text-lg font-[200]">
+                {" "}
+                <CiMobile2  color="LIGHTBLUE" size={25} />
+              Mobile App Development
+              </span>
+
+              <button
+                className="font-[100] text-lg flex justify-center items-center gap-5 bg-[#02007f]
+          px-2 py-2 rounded-sm mt-4 w-[9em] text-[white]"
+              >
+                Read More <RiArrowRightLine />
+              </button>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
