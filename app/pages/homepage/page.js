6fbd9b2e55f7e5c589f7538384d8fc158de5baef
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2025-04-04 14:08:19 +0100

    Update page.js

--- app/pages/homepage/page.js
+++ app/pages/homepage/page.js
@@ -7,9 +7,9 @@ import style from "../../../styles/pages/homepage.module.scss";
 
 const HomePage = () => {
   return (
-    <div className={style.mainContainer}>
+    <div className={style.mainContainer} id="homepage">
       <div className={style.personalContainer}>
-        <Typography className="text-md text-[#12182b]">Hello! I'm</Typography>
+        <Typography className="text-md text-[#12182b]">Hello! I&apos;m</Typography>
         <Typography className={style.nameBox}>THEOPHILUS AKOWUAH</Typography>
         <Typography className={style.skillBox}>
           A SOFTWARE <span style={{ color: "brown" }}> DEVELOPER </span>
@@ -28,14 +28,14 @@ const HomePage = () => {
         <p className={style.detailBox}>
           I develop innovative solutions for WEB and MOBILE platforms, crafting
           seamless user experiences. Need an expert in this technological field?
-          I'm here to bring your ideas to life. Don't hesitate to reach out and
-          let's build something amazing together! 🚀
+          I&apos;m here to bring your ideas to life. Don&apos;t hesitate to reach out and
+          let&apos;s build something amazing together! 🚀
         </p>
-        <Button className={style.talk}>Let's Vibe</Button>
+        <Button className={style.talk}>Let&apos;s Vibe</Button>
       </div>
 
       <div className={style.profileImageBox}>
-        <Image className={style.profileImage} src={PersonalImage} />
+        <Image className={style.profileImage} src={PersonalImage} alt="Profile Image" />
       </div>
     </div>
   );
